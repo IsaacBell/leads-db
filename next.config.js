5c546842d9642666270d0cd2ed1709419167a800
Isaac Bell <isaacbell388@gmail.com>
2024-01-28 14:02:49 +0000

    feat: add top level awaits

--- next.config.js
+++ next.config.js
@@ -1,5 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  webpack: (config) => {
+    // this will override the experiments
+    config.experiments = { ...config.experiments, topLevelAwait: true };
+    // this will just update topLevelAwait property of config.experiments
+    // config.experiments.topLevelAwait = true 
+    return config;
+  },
   rewrites: async () => {
     return [
       {
