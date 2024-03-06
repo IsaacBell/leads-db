2249a3a917fed3b79e70dfea292db0a56dfc021c
Isaac Bell <isaacbell388@gmail.com>
2024-03-06 09:07:01 +0000

    feat: snackbar

--- app/page.tsx
+++ app/page.tsx
@@ -3,11 +3,14 @@
 import Image from 'next/image'
 import Link from 'next/link'
 import { useState } from 'react'
+import { useSnackbar } from 'notistack';
 import staticData from '@/utils/staticData'
 
 export default function Home() {
+  const { enqueueSnackbar } = useSnackbar();
+
   const [email, setEmail] = useState<string>('')
-  const [country, setCountry] = useState<string>('')
+  const [country, setCountry] = useState<string>('United States')
   const [industries, setIndustries] = useState<string[]>([])
 
   const handleSubmit = async (e) => {
@@ -28,13 +31,17 @@ export default function Home() {
   
       if (response.ok) {
         const data = await response.json();
-        console.log(data); // Handle the response data as needed
+        console.log(data);
+        enqueueSnackbar('Form submitted successfully!', { variant: 'success' });
+
+
         // Reset form fields
         setEmail('');
         setCountry('');
         setIndustries([]);
       } else {
         console.error('Error:', response.statusText);
+        enqueueSnackbar('An error occurred. Please try again.', { variant: 'error' });
       }
     } catch (error) {
       console.error('Error:', error);
@@ -46,7 +53,10 @@ export default function Home() {
       <main className="flex min-h-screen flex-col items-center justify-center p-24">
         <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
           <div className="text-center mb-8">
-            <h1 className="text-4xl font-bold mb-4">LeadsDB</h1>
+            <div className="flex justify-center mb-0">
+              <Image src="/LeadsDB-Logo.png" width={300} height={300} alt="LeadsDB Logo" />
+            </div>
+            <h1 className="mt-0 text-4xl font-bold mb-4">Discover new contacts</h1>
             <p className="text-lg text-gray-600">
               Searching for new leads? Enter your target search parameters and receive weekly updates directly to your email.
             </p>
@@ -75,10 +85,12 @@ export default function Home() {
                 </label>
                 <select
                   id="country"
-                  className="block w-full p-2 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                   value={country}
+                  onChange={(e) => setCountry(e.target.value)}
+                  className="block w-full p-2 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                 >
-                  {staticData.countries.map(country => <option key={country} value={country}>{country}</option> )}
+                  {staticData.countries.map(
+                    country => <option key={country}>{country}</option> )}
                 </select>
               </div>
             </div>
